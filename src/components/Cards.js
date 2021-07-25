cdcf8bb24260a4c02d1bf92b898a15fe9450ce83
Dylan648 <bolterd01@student.psdr3.org>
2021-07-25 13:33:17 -0500

    changed cards + shrank mobile banners

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -24,20 +24,20 @@ function Cards() {
                     </ul>
                     <ul className="cards__items">
                         <CardItem 
-                            src="images/img-9.jpg"
-                            text="Explore the waterfall"
+                            src="images/img-1.jpg"
+                            text="Climb to the peaks of the beautiful Rocky Mountains"
                             label="Adventure"
                             path='/services'
                         />
                         <CardItem 
-                            src="images/img-2.jpg"
-                            text="Travel to the beach of Bali"
+                            src="images/img-3.jpg"
+                            text="Fish in the Atlantic on a private boat"
                             label="Luxury"
                             path='/services'
                         />
                         <CardItem 
-                            src="images/img-2.jpg"
-                            text="Travel to the beach of Bali"
+                            src="images/img-6.jpg"
+                            text="Explore the energetic city of New York"
                             label="Luxury"
                             path='/services'
                         />
